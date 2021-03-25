c16a87db648e2b6e36f86785e0333434a69eae60
b6320502452 <pavich.s@ku.th>
2021-03-25 19:12:35 +0700

    .

--- CubeFreeNumbers.c
+++ CubeFreeNumbers.c
@@ -26,20 +26,9 @@ int main()
     {
         for(int i=1;i<=n;i++)
         {
-            if(ckCube(i))
+            if(!ckCube(i))
             {
-                continue;
-            }
-            else
-            {
-                for(int j=1;j<=i;j++)
-                {
-                    if(i%(j*j*j)==0)
-                    {
-                        count++;
-                        break;
-                    }
-                }
+                count++;
             }
         }
         printf("%d",count);
